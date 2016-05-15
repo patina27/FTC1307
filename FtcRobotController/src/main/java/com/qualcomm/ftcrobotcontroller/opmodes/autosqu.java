766ba873c7364e3cc670917e3f4a3eeb69940f84
patina27 <7.pt.wang@gmail.com>
2016-05-15 22:46:16 +0800

    Update Code

---
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/autosqu.java
@@ -0,0 +1,76 @@
+package com.qualcomm.ftcrobotcontroller.opmodes;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
+
+public class autosqu extends LinearOpMode {
+
+
+	DcMotor motorRight;
+	DcMotor motorLeft;
+	DcMotor motorRightt;
+	DcMotor motorLeftt;
+	Servo tail;
+	Servo handl;
+	Servo handr;
+	public autosqu() {
+	}
+
+	void initialize () {
+		motorRight = hardwareMap.dcMotor.get("motor_1");
+		motorLeft = hardwareMap.dcMotor.get("motor_2");
+		motorRightt = hardwareMap.dcMotor.get("motor_3");
+		motorLeftt = hardwareMap.dcMotor.get("motor_4");
+		tail = hardwareMap.servo.get("servo_3");
+		handl = hardwareMap.servo.get("servo_1");
+		handr = hardwareMap.servo.get("servo_2");
+		motorLeft.setDirection(DcMotor.Direction.REVERSE);
+		motorLeftt.setDirection(DcMotor.Direction.REVERSE);
+		motorRight.setPower(0);
+		motorLeft.setPower(0);
+		motorRightt.setPower(0);
+		motorLeftt.setPower(0);
+	}
+	void Movement() throws InterruptedException {
+		motorRight.setPower(0.5);
+		motorLeft.setPower(0.5);
+		motorRightt.setPower(0.5);
+		motorLeftt.setPower(0.5);
+		for (int i = 1; i <= 10; ++i) {
+			sleep(550);
+			tail.setPosition(1);
+			handl.setPosition(0.3);
+			handr.setPosition(0.6);
+		}
+	}
+
+	void turnr1() throws InterruptedException {
+		motorRight.setPower(0.1);
+		motorLeft.setPower(-0.2);
+		motorRightt.setPower(0.3);
+		motorLeftt.setPower(-0.5);
+		for (int i = 1; i <= 10; ++i) {
+			sleep(380);
+			tail.setPosition(1);
+			handl.setPosition(0.3);
+			handr.setPosition(0.6);
+		}
+	}
+	void end(){
+		motorRight.setPower(0);
+		motorLeft.setPower(0);
+		motorRightt.setPower(0);
+		motorLeftt.setPower(0);
+	}
+	public void runOpMode() throws InterruptedException{
+		initialize();
+		waitForStart();
+		for (int i=1;i<=4;++i) {
+			Movement();
+			turnr1();
+		}
+		end();
+	}
+
+}
